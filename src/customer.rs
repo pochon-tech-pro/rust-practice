352666323473abe7c94936c6db5901972b17d6ad
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-18 17:57:11 +0900

    feat: Customer構造体を作る

--- src/customer.rs
+++ src/customer.rs
@@ -5,3 +5,17 @@ pub struct CustomerRequestDTO {
     pub mail: String,
     pub is_active: bool,
 }
+
+#[derive(Debug)]
+pub struct Customer {
+    id: u32,
+    name: String,
+    mail: String,
+    is_active: bool,
+}
+
+impl Customer {
+    pub fn new(input: CustomerRequestDTO) -> Customer {
+        Customer { id: input.id, name: input.name, mail: input.mail, is_active: input.is_active }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod customer;
 
 fn main() {
-    let customer = customer::CustomerRequestDTO {
+    let input = customer::CustomerRequestDTO {
         id: 1,
         name: String::from("Test"),
         mail: "Test@example.com".to_string(),
         is_active: true,
     };
+    println!("{:?}", input);
+
+    let customer = customer::Customer::new(input);
     println!("{:?}", customer);
 }
