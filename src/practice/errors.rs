54f5575c7e8e34084669565f41cea6c9f06cf317
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-23 00:40:04 +0900

    feat: 複数のErrorに対応できるように、Displayをimplする

--- src/practice/errors.rs
+++ src/practice/errors.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 use std::fs::File;
 
 #[derive(Debug)]
@@ -15,6 +17,26 @@ fn basic_error() -> Result<(), MyError> {
     return Err(MyError::Error1(404));
 }
 
+// https://cha-shu00.hatenablog.com/entry/2020/12/08/060000
+impl Display for MyError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        use self::MyError::*;
+        match self {
+            Error1(ui) => write!(f, "Error1: {}", ui),
+            Error2(str) => write!(f, "Error2: {}", str),
+        }
+    }
+}
+
+impl Error for MyError {}
+
+fn multi_error() -> Result<(), Box<dyn Error>> {
+    // 1つめのError
+    // File::open("sample_sample_ohh_sample.txt")?;
+    // 2つめのError
+    Err(MyError::Error2("Error!!".to_string()))?;
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,5 +50,11 @@ mod tests {
             println!("Result型のErr: {:?}", result);
             println!("Errの中身抽出: {:?}", my_error);
         }
+    }
+
+    #[test]
+    fn error_case2() {
+        let result = multi_error();
+        println!("{:?}", result);
     }
 }
