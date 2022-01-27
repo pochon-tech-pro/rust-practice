d8ada70a061d2eed643014e283815fcf0cd34221
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-27 13:28:46 +0900

    feat: Repositoryの実装
    Ericの口座サービスをRustでかく

--- src/practice/domain_service.rs
+++ src/practice/domain_service.rs
@@ -6,15 +6,16 @@ use anyhow::Result;
 // -------------------------
 // Domain Model
 // -------------------------
-#[derive(Debug, PartialEq, Eq, Clone, new)]
-struct Id(String);
+// HashMapのキーにするために、Hashトレイトを継承させる
+#[derive(Debug, PartialEq, Eq, Clone, new, Hash)]
+pub struct Id(String);
 
 #[derive(Debug, PartialEq, Eq, Clone, new)]
-struct BankName(String);
+pub struct BankName(String);
 
 
 #[derive(Debug, PartialEq, Eq, Clone, new)]
-struct Accounts {
+pub struct Accounts {
     id: Id,
     name: BankName,
     balance: u16,
@@ -23,10 +24,32 @@ struct Accounts {
 // -------------------------
 // Repository (IF)
 // -------------------------
-trait IAccountsRepository {
-    fn save(&self, accounts: Accounts) -> Result<()>; // Resultは処理失敗の可能性考慮
+trait IAccountsRepository{
+    fn save(&self, accounts: Accounts) -> Result<()>;
+    // Resultは処理失敗の可能性考慮
     fn of(&self, id: Id) -> Result<Option<Accounts>>; // Resultは処理失敗or存在しない可能性考慮
 }
+
+// -------------------------
+// Repository (Impl)
+// -------------------------
+#[derive(Clone, new)]
+pub struct InMemoryAccountsRepository {
+    store: HashMap<Id, Accounts>,
+}
+
+impl IAccountsRepository for InMemoryAccountsRepository {
+    fn save(&self, accounts: Accounts) -> Result<()> {
+        let mut store = self.store.clone();
+        store.insert(accounts.id.clone(), accounts);
+        return Ok(());
+    }
+
+    fn of(&self, id: Id) -> Result<Option<Accounts>> {
+        todo!()
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -37,5 +60,13 @@ mod tests {
         let id = Id::new("X9111".to_string());
         let bank_name = BankName::new("X9111".to_string());
         println!("{:?}, {:?}", id, bank_name);
+    }
+
+    #[test]
+    fn repository() {
+        let repo = InMemoryAccountsRepository::new(HashMap::new());
+        let id = Id::new("X9111".to_string());
+        let bank_name = BankName::new("X9111".to_string());
+        let accounts = Accounts::new(id, bank_name, 1000);
     }
 }
