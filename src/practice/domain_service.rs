4f3d05a2568c0c369f872ac8cfee0063b667ba96
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-29 10:19:49 +0900

    feat: InMemoryなRepository参照が行えるように実装
    Ericの口座サービスをRustでかく

--- src/practice/domain_service.rs
+++ src/practice/domain_service.rs
@@ -1,20 +1,22 @@
+use std::borrow::BorrowMut;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use derive_new::new;
+use derive_getters::Getters;
 use anyhow::Result;
 
 // -------------------------
 // Domain Model
 // -------------------------
 // HashMapのキーにするために、Hashトレイトを継承させる
-#[derive(Debug, PartialEq, Eq, Clone, new, Hash)]
+#[derive(Debug, PartialEq, Eq, Clone, new, Hash, Default)]
 pub struct Id(String);
 
-#[derive(Debug, PartialEq, Eq, Clone, new)]
+#[derive(Debug, PartialEq, Eq, Clone, new, Default)]
 pub struct BankName(String);
 
 
-#[derive(Debug, PartialEq, Eq, Clone, new)]
+#[derive(Debug, PartialEq, Eq, Clone, new, Getters, Default)]
 pub struct Accounts {
     id: Id,
     name: BankName,
@@ -24,7 +26,7 @@ pub struct Accounts {
 // -------------------------
 // Repository (IF)
 // -------------------------
-trait IAccountsRepository{
+trait IAccountsRepository {
     fn save(&self, accounts: Accounts) -> Result<()>;
     // Resultは処理失敗の可能性考慮
     fn of(&self, id: Id) -> Result<Option<Accounts>>; // Resultは処理失敗or存在しない可能性考慮
@@ -35,21 +37,25 @@ trait IAccountsRepository{
 // -------------------------
 #[derive(Clone, new)]
 pub struct InMemoryAccountsRepository {
-    store: HashMap<Id, Accounts>,
+    // インメモリ上で参照を共有したいのでArcMutexで包む
+    store: Arc<Mutex<HashMap<Id, Accounts>>>,
 }
 
 impl IAccountsRepository for InMemoryAccountsRepository {
     fn save(&self, accounts: Accounts) -> Result<()> {
-        let mut store = self.store.clone();
+        let store = self.store.clone(); // consider using a `let` binding to create a longer lived value
+        let mut store = store.lock().unwrap();
         store.insert(accounts.id.clone(), accounts);
         return Ok(());
     }
 
     fn of(&self, id: Id) -> Result<Option<Accounts>> {
-        todo!()
+        let store = self.store.clone();
+        let store = store.lock().unwrap();
+        let target = store.get(&id).cloned(); // cloned: Optionの中のAccountsがCloneされる
+        return Ok(target)
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -64,9 +70,13 @@ mod tests {
 
     #[test]
     fn repository() {
-        let repo = InMemoryAccountsRepository::new(HashMap::new());
+        let repo = InMemoryAccountsRepository::new(Arc::new(Mutex::new(HashMap::new())));
         let id = Id::new("X9111".to_string());
         let bank_name = BankName::new("X9111".to_string());
-        let accounts = Accounts::new(id, bank_name, 1000);
+        let accounts = Accounts::new(id.clone(), bank_name.clone(), 1000);
+        repo.save(accounts.clone());
+        let target = repo.of(id).unwrap().unwrap();
+        println!("before {:?}, after {:?}", &accounts, &target);
+        assert_eq!(accounts, target);
     }
 }
